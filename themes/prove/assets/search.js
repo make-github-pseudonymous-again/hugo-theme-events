bade1177b762d4b282fc35bcd56368bb9d5a4c0e
Aurélien Ooms <aurelien.ooms@gmail.com>
2019-07-03 17:00:41 +0200

    :art: style(search): Make search result look good.

--- themes/prove/assets/search.js
+++ themes/prove/assets/search.js
@@ -1,7 +1,45 @@
+var ICONS = {
+    'collection': 'class' ,
+    'item' : 'notes' ,
+    'author': 'face' ,
+    'tag' : 'label' ,
+} ;
+
+function statusAndDate ( date , status ) {
+    if (status === "CANCELLED") return `<s>${date}</s><span class="red-text"> (CANCELLED)</span>` ;
+    else if (status === "TENTATIVE") return `${date}<span class="orange-text"> (TENTATIVE DATE AND TIME)</span>`;
+    return date;
+}
+
+function getKind ( url ) {
+    const parts = url.slice(1,-1).split('/');
+
+    if (parts.length === 1) return 'collection' ;
+
+    if (parts[0] === 'authors') return 'author' ;
+    if (parts[0] === 'tags') return 'tag' ;
+
+    return 'item' ;
+}
+
 function matchToHTML ( match ) {
 	const href = match.document.link;
-	const text = `${match.document.link} - ${match.document.title} (${match.result.score})`;
-	return `<a class="collection-item" href="${href}">${text}</a>`;
+	const title = `${match.document.link} - ${match.document.title} (${match.result.score})`;
+	const subtitle = match.document.date ? statusAndDate(match.document.date, match.document.status) : '';
+	const text = match.document.summary;
+	const kind = getKind(match.document.link);
+	const icon = ICONS[kind];
+	let avatar = `<i class="material-icons circle">${icon}</i>` ;
+	const thumbnail = match.document.thumbnail;
+	if ( thumbnail ) {
+		avatar = `<img src="${thumbnail.src}" alt="${thumbnail.caption}" class="circle">` ;
+	}
+	return `<a class="collection-item avatar" href="${href}">
+	  ${avatar}
+	  <span class="title">${title}</span>
+	  <p>${subtitle}${text ? '<br>'+text : ''}</p>
+	  <span class="secondary-content"><i class="material-icons">${icon}</i></span>
+	</a>`;
 }
 
 function measure ( what , callback ) {
@@ -47,6 +85,9 @@ function initSearch ( ) {
 		}
 
 		results.innerHTML = '<div class="collection">' + resultHTML + '</div>' ;
+
+		if (renderMathInElement) renderMathInElement(results);
+
 	}
 
 	// Configure search button to show and focus search input
